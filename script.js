e8fe95d72625edef919708f141eaf895b2835c03
littleshistar <116385291+littleshistar@users.noreply.github.com>
2023-10-05 13:09:21 -0300

    Add new canvas button

--- script.js
+++ script.js
@@ -20,4 +20,34 @@ function createCanvas(number) {
     }
 }
 
+const newCanvasBtn = document.querySelector("#newcanvas");
+newCanvasBtn.addEventListener("click", newCanvas = () => {
+  const row = document.querySelector(".row");
+  let number = row.childElementCount;
+
+  let canvasSize= prompt("How many rows and columns would you like in your new canvas?", "16");
+  let answerType = isNaN(canvasSize);
+
+  console.log(answerType);
+  console.log(canvasSize);
+
+  if (answerType == false) {
+    if (canvasSize > 100) {
+      alert("Please input a number lower than 100 to prevent crashes.")
+    } else {
+      createCanvas(canvasSize);
+      for (let i = 0; i < number; i++) {
+        for (let i = 0; i < number; i++) {
+          const gridbox = document.querySelector(".gridbox");
+          gridbox.remove();
+        }
+        const row = document.querySelector(".row");
+        row.remove();
+      }
+    }
+  } else if (answerType == true) {
+      alert("Please input a number.\nExample: 16");
+  }
+})
+
 createCanvas(16);
